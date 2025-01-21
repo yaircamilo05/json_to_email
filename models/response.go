17240417662f1d41a01c68415db39496ce845d4b
yaircamilo05 <yair.sanchez21369@ucaldas.edu.co>
2025-01-20 21:15:40 -0500

    Remove unused TookDetail struct from response model to streamline code

--- models/response.go
+++ models/response.go
@@ -1,13 +1,5 @@
 package models
 
-type TookDetail struct {
-	Total            int `json:"total"`
-	IdxTook          int `json:"idx_took"`
-	WaitQueue        int `json:"wait_queue"`
-	ClusterTotal     int `json:"cluster_total"`
-	ClusterWaitQueue int `json:"cluster_wait_queue"`
-}
-
 type SearchResponse struct {
 	Hits        []Email `json:"hits"`
 	ScanRecords int     `json:"scan_records"`
